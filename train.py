65c15b2789b927412a51420fd111f1fed93c77f7
Chuan <nauhcnay@gmail.com>
2022-07-30 23:36:25 -0400

    try to fix bug

--- train.py
+++ train.py
@@ -236,7 +236,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     args = get_args()
     
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     logging.info(f'Using device {device}')
 
     net = UNet(in_channels=3, out_channels=1, bilinear=True)
