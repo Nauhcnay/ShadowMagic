2ba4f004a3b317b3b8e1c68f09e02e654c39844b
Chuan Yan <nauhcnay@gmail.com>
2023-07-07 16:17:30 +0800

    bug fix

--- utils/dataset.py
+++ utils/dataset.py
@@ -231,7 +231,7 @@ class BasicDataset(Dataset):
         dice = np.random.uniform()
         size = self.resize
         if dice < random_resize:
-            size = int(size / 1.5)
+            size = int(size / 1.2)
         # we resize the shorter edge to the target size
         if h > w:
             ratio =  h / w
