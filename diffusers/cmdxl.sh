6f98572f5397472f3f125e236d9c61b5a560720b
Chuan Yan <nauhcnay@gmail.com>
2023-12-01 00:42:01 -0500

    sdxl batch train updated

--- diffusers/cmdxl.sh
+++ diffusers/cmdxl.sh
@@ -9,11 +9,12 @@ accelerate launch train_controlnet_sdxl.py \
  --resolution=1024 \
  --learning_rate=1e-5 \
  --max_train_steps=15000 \
+ --tracker_project_name=ShadowMagicSDXL\
  --cache_dir=dataset/sdxl\
  --validation_image "./validation/sd1.5/image11_color.png" "./validation/sd1.5/image11_line.png" "./validation/sd1.5/image12_color.png" "./validation/sd1.5/image12_line.png"\
  --validation_prompt "add shadow from left lighting and remove color" "add shadow from left lighting" "add shadow from left lighting and remove color" "add shadow from right lighting"\
  --validation_steps=250 \
- --train_batch_size=1 \
+ --train_batch_size=2 \
  --gradient_accumulation_steps=4 \
  --report_to="wandb" \
  --seed=42 \
